febfa9c351a1006925ca1383af3b2ea58b7ecdb3
DarshanLevi <s2darshan2@gmail.com>
2025-06-26 16:34:35 +0530

    update:1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import BlurText from "./BlurText";
+import SplitText from "./SplitText";
+import Aurora from "./Aurora";
+import { div } from "framer-motion/client";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleBlurComplete = () => {
+    console.log("BlurText animation completed!");
+  };
+
+  const handleSplitTextComplete = () => {
+    console.log("SplitText animation completed!");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="aurora-wrapper">
+        <Aurora
+          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+          blend={0.5}
+          amplitude={1.0}
+          speed={1.5}
+          height={123}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="blur-wrapper">
+        <BlurText
+          text="Isn't this so cool?!"
+          delay={150}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleBlurComplete}
+          className="blur-text"
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="split-wrapper">
+        <SplitText
+          text="Hello, GSAP!"
+          className="split-text"
+          delay={40}
+          duration={0.6}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+          onLetterAnimationComplete={handleSplitTextComplete}
+        />
+      </div>
+  </div>
+
+  );
 }
 
-export default App
+export default App;

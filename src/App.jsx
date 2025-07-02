2d225b0d192ffde3a207700aed9f5a5f97b3f054
DarshanLevi <s2darshanrider@gmail.com>
2025-07-02 18:54:41 +0530

    update: 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
+import LoopingShapes from "./components/LoopingShapes";
+import PixelTrail from './components/PixelTrail';
+
 import "./App.css";
 
 function App() {
@@ -8,6 +11,17 @@ function App() {
     <div>
       <Navbar />
       <HeroSection />
+      <LoopingShapes />
+      <div style={{ height: '500px', position: 'relative', overflow: 'hidden'}}>
+  <PixelTrail
+    gridSize={100}
+    trailSize={0.03}
+    maxAge={450}
+    interpolate={1.5}
+    color="#cfcfcf"
+    gooeyFilter={{ id: "custom-goo-filter", strength: 1 }}
+  />
+</div>
       {/* You can add more sections here */}
     </div>
   );

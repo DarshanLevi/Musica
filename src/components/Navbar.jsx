b8387e955216f7c2d573b585fd3ced3c25ad521c
DarshanLevi <s2darshan2@gmail.com>
2025-07-16 14:01:27 +0530

    update: 10 (media query)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import musicaLogo from "../assets/Musica.svg";
 import ClickSpark from "./ClickSpark";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+
   return (
-        <ClickSpark
+    <ClickSpark
       sparkColor="rgb(229, 151, 40)"
       sparkSize={10}
       sparkRadius={15}
       sparkCount={8}
       duration={300}
     >
-    <nav className="navbar">
-      <div className="nav-wrap">
-        <img src={musicaLogo} alt="MUSICA Logo" className="logo-img" />
-        <div className="navbar-logo">MUSICA</div>
-      </div>
+      <nav className="navbar">
+        <div className="nav-wrap">
+          <img src={musicaLogo} alt="MUSICA Logo" className="logo-img" />
+          <div className="navbar-logo">MUSICA</div>
+        </div>
 
-      <ul className="navbar-links">
-        <li>
-          <a href="#browse">Browse events</a>
-        </li>
-        <li>
-          <a href="#help">Help</a>
-        </li>
-        <li>
-          <a href="#login">Log In</a>
-        </li>
-        <li>
-          <button className="get-app-button">Get App</button>
-        </li>
-      </ul>
-    </nav>
+        <button className="hamburger" onClick={toggleMenu}>
+          {menuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+        </button>
+
+        <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
+          <li>
+            <a href="#browse">Browse events</a>
+          </li>
+          <li>
+            <a href="#help">Help</a>
+          </li>
+          <li>
+            <a href="#login">Log In</a>
+          </li>
+          <li>
+            <button className="get-app-button">Get App</button>
+          </li>
+        </ul>
+      </nav>
     </ClickSpark>
   );
 };

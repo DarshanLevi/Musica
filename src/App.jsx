2034b0b99f5c5ae8e9aa365a7573647922666e3c
DarshanLevi <s2darshanrider@gmail.com>
2025-08-01 14:11:39 +0530

    update: 12.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import TrendingAndStepsSection from "./components/TrendingAndStepsSection";
 import Footer from "./components/Footer";
+import CustomCursor from "./components/CustomCursor";
 
 import "./App.css";
 
@@ -9,16 +10,15 @@ function App() {
   return (
     <>
       <div className="img-wrapper">
+        <CustomCursor />
         <Navbar />
         <HeroSection />
       </div>
-
 
       <div className="glass-wrap">
         <TrendingAndStepsSection />
         <Footer />
       </div>
-    
     </>
   );
 }
---
+++ src/components/CustomCursor.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import "./CustomCursor.css";
+
+const CustomCursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const move = (e) => setPosition({ x: e.clientX, y: e.clientY });
+    window.addEventListener("mousemove", move);
+    return () => window.removeEventListener("mousemove", move);
+  }, []);
+
+  return (
+    <div
+      className="custom-cursor"
+      style={{ left: position.x, top: position.y }}
+    />
+  );
+};
+
+export default CustomCursor;
